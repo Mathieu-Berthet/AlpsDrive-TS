5270de34abd1f2315efeb569fe93b4cb668b3e7f
Mathieu <berthetmathieu@free.fr>
2023-04-26 11:21:38 +0200

    first request in typescript

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
 console.log("coucou");
+
+import http from 'http';
+import app from './app';
+
+
+app.set('port', process.env.PORT || 3000);
+const server : any = http.createServer(app);
+
+server.listen(process.env.PORT || 3000);
